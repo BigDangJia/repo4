db9c3cb7ef6ef2a453c797d1158d17bef8ab2004
BigDangJia <786865925@qq.com>
2020-01-08 09:01:20 +0800

    第二次提交项目

--- src/com/peng/test.java
+++ src/com/peng/test.java
@@ -11,4 +11,7 @@ public class test {
         System.out.println("Hello git Project");
         System.out.println("Hello git Project");
     }
+    public void test(){
+        System.out.println("我的天~~~~~~");
+    }
 }
